d1fe2bf9403bbf885f5696d5fcdfa15f928fac6b
Taras Bulba <bulbataras884@gmail.com>
2022-07-27 18:04:32 +0100

    allowed hosts tweaked

--- borders/settings.py
+++ borders/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [127.0.0.1′, ‘.herokuapp.com’]
+ALLOWED_HOSTS = ['belarusborders.herokuapp.com']
 
 
 # Application definition
